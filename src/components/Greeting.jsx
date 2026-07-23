3cc99c114109bbfbd2726f9c5cdccdab21834a5c
shebeebk10-hue <shebeebk10@gmail.com>
2026-07-24 00:17:17 +0530

    Updated Spotify clone UI

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -2,7 +2,7 @@ function Greeting() {
   return (
     <div className="px-8 pt-2 pb-6">
       <h1 className="text-4xl font-bold text-white">
-        Good Morning
+        Good Morning!
       </h1>
     </div>
   );
